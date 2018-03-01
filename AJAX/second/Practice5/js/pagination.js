8a5ef09b7e3baa7aff5e173ca031ddd0dff79159
duyanh <alyssachia1992@gmail.com>
2018-03-01 13:52:25 +0700

    Update Pagination Ajax - 010320181352

--- AJAX/second/Practice5/js/pagination.js
+++ AJAX/second/Practice5/js/pagination.js
@@ -1,25 +1,126 @@
 $(function(){
-	//var start = 0;
+	var totalPage = 0;
 	var itemPerPage = 4;
 	var page = 1;
 	
-	$('.show-prd').load('load_data.php' ,{'itemPerPage':itemPerPage, 'page':page}, function(res){
-			var res2 = JSON.parse(res);
-			var totalPage = res2.page;
-		$('#pageInfo').text('Show 1 of '+res2.totalPage);
+	function loadData(page){
+		$.ajax({
+			url : 'load_data.php',
+			type : 'post',
+			dataType : 'json',
+			data : {'itemPerPage':itemPerPage, 'page':page, 'action':'list'},
+			success : function(res){
+				 if(res.length > 0){
+					totalPage = res.totalPage;
+					$('.show-prd').empty();
+					var xhtml = '';
+					xhtml+= '<div class="container"><div class="row">';
+					
+					$.each(res, function(k,v){
+						xhtml+= '<div class="col-md-3 text-center prd">';
+						xhtml+= '<img src="images/'+v.anh_sp+'" alt="" width="150px" height="200px"/>';
+						xhtml+= '<p><b>'+v.ten_sp+'</b></p>';
+						xhtml+= '<p style="color:red; font-weight:bold;">'+v.gia_sp+'</p>';
+						xhtml+= '</div>';
+					}); 
+					
+					
+					xhtml+= '</div></div>';
+				 }
+				 $('.show-prd').append(xhtml);
+			}
+		});
+		
+		if(page == 1){
+			$('#prev').attr('disabled', 'disabled');
+		}
+		else{
+			$('#prev').removeAttr('disabled');
+		}
+		
+		checkNext(page, totalPage);
+	}
+	
+	function clickPrev(page, totalPage){
+		if(page != 1){
+			$('#prev').click(function(){
+				page-= 1;
+				loadData(page);
+				$('#pageInfo').text('Show '+(page)+' of '+totalPage);
+				$('#sltPage option[value='+page+']').attr('selected','selected');
+			});
+		}
+	}
+	
+	function clickNext(page, totalPage){
+		if(page != totalPage){
+			$('#next').click(function(){
+				page+= 1;
+				loadData(page);
+				$('#pageInfo').text('Show '+(page)+' of '+totalPage);
+				$('#sltPage option[value='+page+']').attr('selected','selected');
+			});
+		}
+	}
+	
+	function checkNext(page, totalPage){
+		if(page == totalPage){
+			$('#next').attr('disabled', 'disabled');
+		}
+		else{
+			$('#next').removeAttr('disabled');
+		}
+	}
+	
+	$.ajax({
+		url : 'load_data.php',
+		type : 'post',
+		dataType : 'json',
+		data : {'itemPerPage':itemPerPage, 'page':page, 'action':'count'},
+		success : function(res){
+			//var res2 = JSON.parse(res);
+			totalPage = res.totalPage;
+			page = res.page;
+			$('#pageInfo').text('Show 1 of '+totalPage);
+			
+			for(var i=1; i<=totalPage; i++){
+				$('#sltPage').append('<option value="'+i+'">Page '+i+'</option>');
+			}
+		}
 	});
+	
+	loadData(page);
 	
 	$('#sltPage').change(function(){
 		var page = $(this).val();
 		$.ajax({
 			url : 'load_data.php',
 			type : 'post',
-			data : {'itemPerPage':itemPerPage, 'page':page},
+			dataType : 'json',
+			data : {'itemPerPage':itemPerPage, 'page':page, 'action':'count'},
 			success : function(res){
-				 //$('.show-prd').empty();
-				 //$('.show-prd').append(res);
-				console.log(res);
+				totalPage = res.totalPage;
+				page = res.page;
+				$('#pageInfo').text('Show '+page+' of '+totalPage);
+				
+				loadData(page);
+				clickPrev(page, totalPage);
+				
+				clickNext(page, totalPage);
+				checkNext(page, totalPage);
+				//clickNext(page, totalPage);
 			}
 		});
+		
+		//checkNext(page);
+		
+		// loadData(page);
+		// console.log(totalPage);
+		// clickPrev(page, totalPage);
 	});
+	
+	// clickNext(page, totalPage);
+	// clickPrev(page, totalPage);
+	// checkNext(page, totalPage);
+	//clickNext(page, totalPage);
 });
--- AJAX/second/Practice5/load_data.php
+++ AJAX/second/Practice5/load_data.php
@@ -1,35 +1,46 @@
 <?php
 require('functions.php');
 
-if($_POST['itemPerPage'] != null && $_POST['page'] != null){
+if($_POST['itemPerPage'] != null && $_POST['page'] != null && $_POST['action'] != null){
 	$itemPerPage = $_POST['itemPerPage'];
 	$page = $_POST['page'];
+	$action = $_POST['action'];
 	
 	$allData = getAllData();
 	$totalItem = count($allData);
 	$totalPage = ceil($totalItem/$itemPerPage);
-	$start = ($page-1)*$itemPerPage;
 	
-	$dataSection = getElements($allData, $start, $itemPerPage);
 	
-	// $xhtml = '';
-	// $xhtml.= '<div class="container"><div class="row">';
+	if($action == 'count'){
+		$result['totalPage'] = (int)$totalPage;
+		$result['page'] = (int)$page;
+		echo json_encode($result);
+	}
 	
-	// foreach($dataSection as $k=>$v){
-		// $xhtml.= '<div class="col-md-3 text-center prd">
-					// <img src="images/'.$v['anh_sp'].'" alt="" width="150px" height="200px"/>
-					// <p><b>'.$v['ten_sp'].'</b></p>
-					// <p style="color:red; font-weight:bold;">'.$v['gia_sp'].'</p>
-				// </div>';
-	// }
+	if($action == 'list'){
+		$start = ($page-1)*$itemPerPage;
+		
+		$dataSection = getElements($allData, $start, $itemPerPage);
 	
-	// $xhtml.= '</div></div>';
-	//echo $xhtml;
-	
-	$result = array();
-	//$result['xhtml'] = $xhtml;
-	$result['totalPage'] = (int)$totalPage;
-	$result['page'] = (int)$page;
-	echo json_encode($result);
+		// $xhtml = '';
+		// $xhtml.= '<div class="container"><div class="row">';
+		
+		// foreach($dataSection as $k=>$v){
+			// $xhtml.= '<div class="col-md-3 text-center prd">
+						// <img src="images/'.$v['anh_sp'].'" alt="" width="150px" height="200px"/>
+						// <p><b>'.$v['ten_sp'].'</b></p>
+						// <p style="color:red; font-weight:bold;">'.$v['gia_sp'].'</p>
+					// </div>';
+		// }
+		
+		// $xhtml.= '</div></div>';
+		//echo $xhtml;
+		
+		//$result = array();
+		//$result['xhtml'] = $xhtml;
+		// $result['totalPage'] = (int)$totalPage;
+		// $result['page'] = (int)$page;
+		echo json_encode($dataSection);
+	}
 }
 ?>
