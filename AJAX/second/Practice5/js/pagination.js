1a9c63c3c07e0660ec84c3ec5eb144f5ff8317b7
duyanh <alyssachia1992@gmail.com>
2018-03-01 14:23:14 +0700

    Update Pagination Ajax - 010320181422

--- AJAX/second/Practice5/js/pagination.js
+++ AJAX/second/Practice5/js/pagination.js
@@ -38,21 +38,24 @@ $(function(){
 			$('#prev').removeAttr('disabled');
 		}
 		
-		checkNext(page, totalPage);
 	}
 	
 	function clickPrev(page, totalPage){
-		if(page != 1){
-			$('#prev').click(function(){
-				page-= 1;
-				loadData(page);
-				$('#pageInfo').text('Show '+(page)+' of '+totalPage);
-				$('#sltPage option[value='+page+']').attr('selected','selected');
-			});
-		}
+		//alert(1);
+		//console.log(page);
+		// if(page != 1){
+			// $('#prev').click(function(){
+				// page-= 1;
+				// loadData(page);
+				// $('#pageInfo').text('Show '+(page)+' of '+totalPage);
+				// $('#sltPage option[value='+page+']').attr('selected','selected');
+			// });
+		// }
 	}
 	
 	function clickNext(page, totalPage){
+		//console.log(page);
+		//console.log(totalPage);
 		if(page != totalPage){
 			$('#next').click(function(){
 				page+= 1;
@@ -108,19 +111,11 @@ $(function(){
 				
 				clickNext(page, totalPage);
 				checkNext(page, totalPage);
-				//clickNext(page, totalPage);
 			}
 		});
-		
-		//checkNext(page);
-		
-		// loadData(page);
-		// console.log(totalPage);
-		// clickPrev(page, totalPage);
 	});
-	
-	// clickNext(page, totalPage);
+	$('#next').click(clickNext(page, totalPage));
 	// clickPrev(page, totalPage);
 	// checkNext(page, totalPage);
-	//clickNext(page, totalPage);
+	// clickNext(page, totalPage);
 });
